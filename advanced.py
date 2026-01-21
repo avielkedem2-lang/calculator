6833ad07b262095bc4718d0eef2bcaab4f0a9dde
אביאל קדם <avielkedem2@gmail.com>
2026-01-21 16:37:03 +0200

    הוספנו חישוב עצרת

--- advanced.py
+++ advanced.py
@@ -9,3 +9,12 @@ def square_root(n):
     return n ** 0.5
 def modulo(a, b):
     return a % b
+
+
+def factorial(n):
+    if n < 0:
+        return "error: you can't put negative number!"
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
