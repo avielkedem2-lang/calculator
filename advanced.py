96391d51db3c9af4bfab3bc86d849a6231910c1f
אביאל קדם <avielkedem2@gmail.com>
2026-01-21 15:12:16 +0200

    הוספת מודול פעולות מתקדמות

---
+++ advanced.py
@@ -0,0 +1,11 @@
+def power(base, exponent):
+
+    return base ** exponent
+
+def square_root(n):
+
+    if n < 0:
+        return "there is no root for a negative number!"
+    return n ** 0.5
+def modulo(a, b):
+    return a % b
