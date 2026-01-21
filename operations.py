c45fa253db84bbd08f82d929dc5cc3925860cea3
אביאל קדם <avielkedem2@gmail.com>
2026-01-21 14:52:09 +0200

    הוספת פונקציה כפל וחילוק

--- operations.py
+++ operations.py
@@ -4,3 +4,12 @@ def add(a, b):
 def subtract(a, b):
 
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "b can't be 0"
+    return a / b
