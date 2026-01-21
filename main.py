609ddad9f743505e9fc4c6dabcd8a4caff93a478
אביאל קדם <avielkedem2@gmail.com>
2026-01-21 16:11:29 +0200

    שדרוג לגרסה אינטראקטיבית

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from operations import add, subtract, multiply, divide
 from advanced import power, square_root, modulo
+from ui import get_number, show_menu
 
 print("!ברוכים הבאים למחשבון")
 print("====================")
 
+while True:
+    choice = show_menu()
 
-result1 = add(5, 3)
-print(f"5 + 3 = {result1}")
+    if choice == "0":
+        print("goodbye")
+        break
 
+    if choice in ["1", "2", "3", "4", "5"]:
+        num1 = get_number("enter a number")
+        num2 = get_number("enter a second number")
 
-result2 = subtract(10, 4)
-print(f"10 - 4 = {result2}")
+        if choice =="1":
+            print(f"result: {add(num1, num2)}")
 
-print(f"6 * 7 = {multiply(6, 7)}")
-print(f"20 / 4 = {divide(20, 4)}")
-print(f"10 / 0 = {divide(10, 0)}")
+        elif choice == "2":
+            print(f"result {subtract(num1, num1)}")
 
-print("\n --- advanced operations --- ")
-print(f"2 ^ 8 = {power(2, 8)}")
-print(f"√16 = {square_root(16)}")
-print(f"17 % 5 = {modulo(17, 5)}")
+        elif choice == "3":
+            print(f"result {multiply(num1, num2)}")
+
+        elif choice == "4" :
+            print(f"result: {divide(num1, num2)}")
+
+        elif choice == "5":
+            print(f"result: {power(num1, num2)}")
+
+    elif choice == "6":
+        num = get_number("enter a number")
+        print(f"result: {square_root(num)}")
+
+    else:
+        print("your choice it is not a legal")
