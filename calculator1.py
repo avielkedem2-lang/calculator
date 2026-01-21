75810a40b2a080752920020a6bbe42afb3e74b33
אביאל קדם <avielkedem2@gmail.com>
2026-01-21 14:46:28 +0200

    הוספה ל calculator את operations

--- calculator1.py
+++ calculator1.py
@@ -1,2 +1,11 @@
+from operations import add, subtract
 print("!ברוכים הבאים למחשבון")
 print("====================")
+
+
+result1 = add(5, 3)
+print(f"5 + 3 = {result1}")
+
+
+result2 = subtract(10, 4)
+print(f"10 - 4 = {result2}")
