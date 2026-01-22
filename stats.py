e979d46747aea5639d96d3f9a37b18da5556549e
אביאל קדם <avielkedem2@gmail.com>
2026-01-22 09:06:55 +0200

    הוספת מודול סטטיסטיקה

---
+++ stats.py
@@ -0,0 +1,19 @@
+def average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+
+
+def find_max(numbers):
+    if not numbers:
+        return None
+    return max(numbers)
+
+
+
+def find_min(numbers):
+    if not numbers:
+        return None
+    return min(numbers)
+
+
