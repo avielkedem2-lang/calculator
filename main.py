f6e287aa4383f7456cab49bbb54d97c19ffa0373
אביאל קדם <avielkedem2@gmail.com>
2026-01-22 11:56:32 +0200

    main-חיבור היסטוריה ל

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from operations import add, subtract, multiply, divide
 from advanced import *
 from ui import *
+from history import add_to_history, show_history, clear_history
 
 print("!ברוכים הבאים למחשבון")
 print("====================")
@@ -17,26 +18,50 @@ while True:
         num2 = get_number("enter a second number")
 
         if choice =="1":
-            print(f"result: {add(num1, num2)}")
+            result = add(num1, num2)
+            print(f"result: {result}")
+            add_to_history(f"{num1} + {num2}", result)
+
 
         elif choice == "2":
-            print(f"result {subtract(num1, num1)}")
+            result1 = subtract(num1, num1)
+            print(f"result {result1}")
+            add_to_history(f"{num1} - {num2}", result1)
+
 
         elif choice == "3":
-            print(f"result {multiply(num1, num2)}")
+            result2 = multiply(num1, num2)
+            print(f"result {result2}")
+            add_to_history(f"{num1} * {num2}", result2)
+
 
         elif choice == "4" :
-            print(f"result: {divide(num1, num2)}")
+            result3 = divide(num1, num2)
+            print(f"result: {result3}")
+            add_to_history(f"{num1} // {num2}", result3)
+
 
         elif choice == "5":
-            print(f"result: {power(num1, num2)}")
+            result4 = power(num1, num2)
+            print(f"result: {result4}")
+            add_to_history(f"{num1} ** {num2}", result4)
+
 
     elif choice == "6":
         num = get_number("enter a number")
         print(f"result: {square_root(num)}")
+        add_to_history(f"square root of {num} ==", num)
+
 
     elif choice == "7":
         num3 = get_number("enter a number")
         print(f"result: {factorial(num3)}")
+
+    elif choice == "8":
+        show_history()
+
+    elif choice == "9":
+        clear_history()
+
     else:
         print("your choice it is not a legal")
