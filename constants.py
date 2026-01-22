3bd66d0f295a4df714e33b8e2f9e3b34d15509a6
אביאל קדם <avielkedem2@gmail.com>
2026-01-22 14:31:31 +0200

    הוספת מודול קבועים

---
+++ constants.py
@@ -0,0 +1,9 @@
+PI = 3.14159265359
+E = 2.71828182846
+GOLDEN_RATIO = 1.61803398875
+
+def show_constants():
+    print("\n === קבועים מתמטיים === ")
+    print(f"π {PI} פאי = ")
+    print(f"e {E} אוילך = ")
+    print(f"φ {GOLDEN_RATIO} יחס זהב = ")
