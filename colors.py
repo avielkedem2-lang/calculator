5bb3f15a4cc55288ecdfe9a5cd6cd429fd3d1f03
אביאל קדם <avielkedem2@gmail.com>
2026-01-22 14:20:06 +0200

    הוספת מודול צבעים

---
+++ colors.py
@@ -0,0 +1,16 @@
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+def print_success(message):
+    print(f"{GREEN}{message}{RESET}")
+
+
+def print_error(message):
+    print(f"{RED}{message}{RESET}")
+
+
+def print_result(message):
+    print(f"{BLUE}{message}{RESET}")
